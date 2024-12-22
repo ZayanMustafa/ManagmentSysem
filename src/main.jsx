6ab7158682c6dbd03b357b88155ebbfe22ae1144
ZayanMustafa <mustafazayyan@gmail.com>
2024-12-22 12:50:57 +0500

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <>
+  <NextUIProvider> 
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<App />} ></Route>
+
+      </Routes>
+    </BrowserRouter>
+  </NextUIProvider>
+  </>
+  
 )
